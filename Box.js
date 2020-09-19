f0e1fc743e943bfca10957442c9e502b42767c64
nora-benoy <70559907+nora-benoy@users.noreply.github.com>
2020-09-19 14:57:28 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,28 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
+    }  
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+       
+    World.add(world, this.body);
+  }
+
+  display(){
+
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rectMode(CENTER);
+    fill(135,206,234);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+
+}
+
+
